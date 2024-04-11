ee49c6e6ceee4c30cdb76a2f487c9a98dab15794
Miguel Isack verdadeiro <130250696+MiguelSouza2@users.noreply.github.com>
2024-04-11 15:26:38 -0300

    Add files via upload

---
+++ Herança/exemplo_herança/exemplo_herança/funcionário.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_herança
+{
+    class funcionário : pessoa_física
+    {
+        private string cartao;
+        
+        public void setCartao(string cartao)
+        {
+            this.cartao = cartao;
+        }
+
+        public string getCartao()
+        {
+            return this.cartao;
+        }
+    }
+}
---
+++ Herança/exemplo_herança/exemplo_herança/pessoa_física.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo_herança
+{
+    //O sinal de : demonstra que a classe 'pessoa-_física está HERDANDO o conteúdo da classe pessoa'
+    class pessoa_física : pessoa
+    {
+        private string RG;
+
+        public void setRG(string RG)
+        {
+            this.RG = RG;
+        }
+        public string getRG()
+        {
+            return this.RG;
+        }
+    }
+}
