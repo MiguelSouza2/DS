161c30e710a74a61a7cea8acad2000c87ddd54e4
Miguel Isack verdadeiro <130250696+MiguelSouza2@users.noreply.github.com>
2024-08-07 10:43:48 -0300

    Add files via upload

---
+++ Animais_trabalho_CRUD/Animal.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
+
+
+namespace Animais_trabalho_CRUD 
+{
+    class Animal : conexao
+    {
+        private string nome;
+        private string especie;
+        private string codigo;
+        private string genero;
+        private string patas;
+
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setEspecie(string especie)
+        {
+            this.especie = especie;
+        }
+        public string getEspecie()
+        {
+            return this.especie;
+        }
+        public void setPatas(string patas)
+        {
+            this.patas = patas;
+        }
+        public string getPatas()
+        {
+            return this.patas;
+        }
+        public void setGenero(string genero)
+        {
+            this.genero = genero;
+        }
+        public string getGenero()
+        {
+            return this.genero;
+        }
+        public void setCodigo(string codigo)
+        {
+            this.codigo = codigo;
+        }
+        public string getCodigo()
+        {
+            return this.codigo;
+        }
+
+        // Método para inserir as informações
+        public void inserir()
+        {
+            string query = "INSERT INTO animal(nome_animal, especie_animal, patas_animal, genero_animal)VALUES('" + getNome() + "','" + getEspecie() + "','" + getPatas() + "','" + getGenero() + "')";
+
+            if (this.abrirconexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.fecharconexao();
+            }
+        }
+
+        public void excluir()
+        {
+            string query = "DELETE FROM animal WHERE codigo_animal = '" + getCodigo() + "'";
+            if (this.abrirconexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.fecharconexao();
+            }
+
+        }
+
+        /*
+            Criar um método para consultar
+            Colocar biblioteca Using System.Data
+        */
+        public DataTable Consultar()
+        {
+            this.abrirconexao();
+
+            string mSQL = "SELECT * FROM animal";
+            MySqlCommand cmd = new MySqlCommand(mSQL, conectar);
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            this.fecharconexao();
+            // Retornar a consulta SQL em formato de tabela
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+
+        }
+
+        public void alterar()
+        {
+            string query = "UPDATE animal SET nome_animal = '" + getNome() + "', especie_animal = '" + getEspecie() + "', patas_animal = '" + getPatas() + "', genero_animal = '" + getGenero() + "'  WHERE codigo_animal = '" + getCodigo() + "' ";
+            if (this.abrirconexao() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conectar);
+                cmd.ExecuteNonQuery();
+                this.fecharconexao();
+            }
+        }
+
+    }
+}
---
+++ Animais_trabalho_CRUD/conexao.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Biblioteca para conexão ao MySQL
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Animais_trabalho_CRUD
+
+{
+    class conexao
+    {
+        public MySqlConnection conectar;
+        public string servidor;
+        public string database;
+        public string usuario;
+        public string senha;
+
+        public conexao()
+        {
+            inicializar();
+        }
+
+        public void inicializar()
+        {
+            servidor = "127.0.0.1";
+            // local onde está localizado o banco de dados (online ou local) quando está local 127.0.0.1 ou localhost. Quando estiver online é necessário saber o ip do servidor ou nome do servidor
+            database = "crud_animais";
+            // database do banco de dados que iremos criar futuramente
+            usuario = "root";
+            senha = "";
+            // usuário e senha padrôes (root e senha em branco). Em acesso online necessário colocar usuário e senha de acesso
+
+            string conexaostring;
+            conexaostring = "SERVER=" + servidor + ";" + "DATABASE=" + database + ";" + "UID=" + usuario + ";" + "PASSWORD=" + senha + ";";
+
+            // variável conectar utilizada para localizar e acessar o banco de  dados (local ou online)
+            conectar = new MySqlConnection(conexaostring);
+        }
+
+        // Abertura da conexão
+
+        public bool abrirconexao() //try é um tratamento de erro
+        {                          // O que está dentro do try deve ser executado
+            try                    // O que está dentro do catch é a exceção do try
+            {                      // Caso o try não execute ou não funcione será direcionado para o catch
+                conectar.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        System.Windows.Forms.MessageBox.Show("Não foi possível conectar");
+                        break;
+                    case 1045:
+                        System.Windows.Forms.MessageBox.Show("Usuário e senha inválidos");
+                        break;
+                }
+                return false;
+            }
+        }
+        // fechar conexão   
+
+        public bool fecharconexao()
+        {
+            try
+            {
+                conectar.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+
+    }
+}
